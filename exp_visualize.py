7f8e7557116cf960a2f5a69a46c02239b9c27041
Eugene233 <agboreugene@gmail.com>
2025-06-23 20:02:15 +0200

    fix flake issue in visualize

--- exp_visualize.py
+++ exp_visualize.py
@@ -9,13 +9,13 @@ from src.Explainer import Explainer
 from src.utils.visualize_hetero_graphs import visualize_hd
 
 vis_dataset = "mutag"
-vis_explainer = Explainer(explainers=[], dataset=vis_dataset, model_name="RGCN")
+vis_explainer = Explainer(explainers = [], dataset = vis_dataset, model_name = "RGCN")
 vis_category = vis_explainer.category
 vis_model = vis_explainer.model
 idx_map = vis_explainer.idx_map
 node_id = vis_explainer.test_idx[0]
 node_idx = node_id.tolist()
-explainer = NodeSubgraphX(vis_model, num_hops=1, num_rollouts=3, shapley_steps=5)
+explainer = NodeSubgraphX(vis_model, num_hops = 1, num_rollouts = 3, shapley_steps = 5)
 feat = vis_model.input_feature()
 explanation, logits = explainer.explain_node(
     vis_explainer.g, feat, node_id, vis_category
